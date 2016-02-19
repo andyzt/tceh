1949535437d18fcf5bede6608f425fc8756d7c3a
Andrey <andy_evl@mail.ru>
2016-02-19 14:47:56 +0300

    my first commit

--- Homework1.py
+++ Homework1.py
@@ -11,64 +11,64 @@ else:
 
 # Infinite loop:
 while True:
-	users_input = input_function(u'Загадка №1: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №1: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №2: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №2: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №3: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №3: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №4: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №4: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №5: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №5: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №6: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №6: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
 
 while True:
-	users_input = input_function(u'Загадка №7: Сколько бит в одном байте?')
+	users_input = input_function('Загадка №7: Сколько бит в одном байте?')
 	if users_input =='8':
 		print('Правильно!')
 		break
-	else
+	else:
 		print('Ответ неверный, попробуйте еще раз')
 		continue
